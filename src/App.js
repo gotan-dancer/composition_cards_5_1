a2cbc3200f57bfe39ccde8e2f9ea1c3da46b9f56
gotan-dancer <eugene.kalugin@gmail.com>
2023-01-14 21:11:59 +0500

    Commit 01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function Img( {img_src, img_alt} )
+{
+    return(
+        <div>
+            <img src={img_src} class="card-img-top" alt={img_alt}></img>
+        </div>
+    );
+}
+
+function Card( {children} )
+{
+    return(
+        <div>{children}</div>
+    );
+}
+
+
+function App() 
+{
+    return(
+        <div>
+            <Card>
+                <div class="card" style="width: 18rem;">
+                    <Img img_src="..." img_alt="..."></Img>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </Card>
+            <Card>
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>          
+            </Card>
+        </div>
   );
 }
 
